19d717e86b1d3f64889615a2b7cf9703bccd0d17
Arjun Yuvaraj <arjunycoding@gmail.com>
2023-05-21 17:47:10 -0400

    I am pretty sure I finsihed, give to dad and ask how to style

--- src/lessons/lesson_one.js
+++ src/lessons/lesson_one.js
@@ -1,22 +1,24 @@
 import React from "react"
 import LetterBtn from "./letter_button";
-import consonants from "../consonants";
 import vowels from "../vowels";
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-
+import "../App.css"
 export default function LessonOne() {
-    const [current_letter, set_current_letter] = React.useState("No Letter Selected")
+    const [current_letter, set_current_letter] = React.useState(false)
     return (
         <div>
+            Similar to English Sanskrit also has vowels, and consonants. Click on a vowel to look at it more in detail. Click on the huge letter to here it!
             <div>
-                <button onClick={() => { set_current_letter("letter") }}>letter.letter</button>
 
                 {vowels.map((letter) => {
-                    <button key={letter.letter} onClick={() => { set_current_letter(letter) }}>{letter.letter}</button>
+                    return (<button key={letter.letter} onClick={() => { set_current_letter(letter) }}>{letter.letter}</button>)
                 })}
             </div>
-            <div><LetterBtn symbol={current_letter.letter} audio_file={current_letter.fileName} /></div>
-        </div>
+            <div>
+                {current_letter ?
+                    <LetterBtn symbol={current_letter.letter} audio_file={current_letter.fileName} huge={true} /> :
+                    <p>Please Select A Letter</p>
+                }
+            </div>
+        </div >
     )
 }
